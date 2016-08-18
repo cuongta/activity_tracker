df2c4d78c93ecd248aeb02474b85b46eac9764e2
Cuong Ta <cuong@mistsys.com>
2016-08-17 23:41:55 -0700

    Initial Commit

---
+++ ActivityMonitor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ActivityMonitor
+//
+//  Created by Cuong Ta on 8/17/16.
+//  Copyright © 2016 cuong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ActivityMonitor/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ActivityMonitor
+//
+//  Created by Cuong Ta on 8/17/16.
+//  Copyright © 2016 cuong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
